4756f8dc77c84a34d93b5e58407ade7260ce2cb5
cx330o <50682900+cx330o@users.noreply.github.com>
2024-10-06 10:00:00 +0900

    次のバスを計算するロジック

---
+++ src/utils/timeHandlers.ts
@@ -0,0 +1,19 @@
+// Re-export all functions from split modules for backward compatibility
+export {
+  toMinutes,
+  timeDifference,
+  isHoliday,
+  isWeekday,
+  getNextDay,
+  getPreviousDay,
+  timeToMinutes,
+  minutesToTime,
+  getDateString,
+  getTimeString,
+  dayIndices,
+  equationOfTime,
+} from "./dateUtils"
+
+export { findNextBuses, getSpecialDateConfig } from "./busUtils"
+
+export { findNextTrains } from "./trainUtils"
