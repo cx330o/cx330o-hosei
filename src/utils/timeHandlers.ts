95cdbb4067eccd3132741588abdca8c305990f8a
cx330o <50682900+cx330o@users.noreply.github.com>
2025-02-02 14:00:00 +0900

    findNextTrains実装

--- src/utils/timeHandlers.ts
+++ src/utils/timeHandlers.ts
@@ -17,4 +17,4 @@ export {
 export { findNextBuses, getSpecialDateConfig } from "./busUtils"
 
 export { findNextTrains } from "./trainUtils"
-// updated: TypeScript¤ÎÐÍ¥¨¥é©`ÐÞÕý
+// updated: findNextTrainsŒg×°
