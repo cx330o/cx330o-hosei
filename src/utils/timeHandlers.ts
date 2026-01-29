354560d9f5c3014e39f90f52cf6696e038abac98
cx330o <50682900+cx330o@users.noreply.github.com>
2026-01-29 14:00:00 +0900

    strict modeの型エラー修正

--- src/utils/timeHandlers.ts
+++ src/utils/timeHandlers.ts
@@ -17,4 +17,4 @@ export {
 export { findNextBuses, getSpecialDateConfig } from "./busUtils"
 
 export { findNextTrains } from "./trainUtils"
-// updated: findNextTrainsŒg×°
+// updated: strict mode¤ÎÐÍ¥¨¥é©`ÐÞÕý
